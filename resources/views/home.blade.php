523987f3f495f0347b08e9c59ae782dbf45b2271
Manucho661 <emmanuelwanyonyi49@gmail.com>
2023-12-22 14:37:49 +0300

    14,22,12,23

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -12,6 +12,39 @@
     @csrf
     <button> Logout</button>
 </form>
+
+<div style="border: 3px solid black">
+<h3>CREATE NEW POST</h3>
+<form action="/create_post" method="POST"> 
+    @csrf
+<input type="text" name="title">
+<textarea name="body" placeholder="body content..."></textarea>
+<button>Save Post</button>
+</form>
+</div>
+
+<div style="border: 3px solid black">
+<h3>All Posts</h3>
+@foreach($posts as $post) 
+
+<div style="background-color: grey; padding: 10px; margin: 10px;">
+   <h2> {{$post['title']}} by {{$post->user->name}}</h2>
+   {{$post['body']}}
+
+<P>  <a href="/edit-post/{{$post->id}}">Edit</a></P>
+
+   <form action="/delete-post/{{$post->id}}" method="POST">
+@csrf
+@method('DELETE')
+<button>DELETE</button>
+</form>
+</div>
+
+   @endforeach
+
+
+</div>
+
     @else
 
     <div style="border 3px solid black">
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 /*
@@ -15,9 +17,20 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('home');
+$posts=[];
+
+if (auth()->check()) {
+    $posts= auth()->user()->UsersCoolPosts() -> latest()->get();
+    
+}
+    return view('home', ['posts'=> $posts]); 
 });
 
 Route::POST('/register', [UserController::class, 'register']);
 Route::POST('/logout', [UserController::class, 'logout']);
 Route::POST('/login', [UserController::class, 'login']);
+// blog post related routes
+Route::POST('/create_post', [PostController::class, 'create_post']);
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
+Route::put('/edit-post/{post}', [PostController::class, 'actuallyUpdatePost']);
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
